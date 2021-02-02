e6baed69f3875c9603ae502ff57b6e2659ffec19
Geetu <geetu7june@gmail.com>
2021-02-02 11:09:00 +0100

    new update to coDe scripts

--- new.sh
+++ new.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export GIT_COMMITTER_DATE="Wed Feb 12 12:01 2021 +0100"
-export GIT_AUTHOR_DATE="Wed Feb 11 11:09 2021 +0100"
+export GIT_COMMITTER_DATE="Wed Feb 04 12:01 2021 +0100"
+export GIT_AUTHOR_DATE="Wed Feb 02 11:09 2021 +0100"
 git add .
 git commit -am " new update to coDe scripts"
 git push --set-upstream origin master
