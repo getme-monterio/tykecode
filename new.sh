512cb94341ebda90c1bf9905de74f9d5753e092c
neetu <singh.neetus@gmail.com>
2021-03-10 12:09:00 +0100

    new update to code scripts

--- new.sh
+++ new.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export GIT_COMMITTER_DATE="Wed Apr 10 12:01 2021 +0100"
-export GIT_AUTHOR_DATE="Wed Apr 10 12:09 2021 +0100"
+export GIT_COMMITTER_DATE="Wed Mar 10 12:01 2021 +0100"
+export GIT_AUTHOR_DATE="Wed Mar 10 12:09 2021 +0100"
 git add .
 git commit -am " new update to code scripts"
 git push --set-upstream origin master
